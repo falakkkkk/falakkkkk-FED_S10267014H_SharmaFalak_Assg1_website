7f94daa6e3287805d3dde684aa88e021afa01cd2
falakkkkk <s10267014@connect.np.edu.sg>
2024-12-01 18:33:06 +0800

    js file

---
+++ login_signup.js
@@ -0,0 +1,12 @@
+// JavaScript to handle the toggle between Sign In and Sign Up forms
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add('right-panel-active');
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove('right-panel-active');
+});
